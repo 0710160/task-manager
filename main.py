3b844d3029e4f311c3fceae97a9c31a0d9e9925f
0710160 <xlvi@mm.st>
2022-03-09 14:24:46 +1300

    Rework entire site to include recipes section and set up code for additional pages as and when suits. Basically reworked to a personal everything site rather than having multiples ones set up.

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from sqlalchemy.orm import relationship
 from time import time
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.exceptions import HTTPException
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from flask_ckeditor import CKEditor, CKEditorField, upload_success, upload_fail
 import os
+import json
 import psycopg2
 
 app = Flask(__name__)
@@ -17,12 +17,14 @@ ckeditor = CKEditor(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['CKEDITOR_SERVE_LOCAL'] = True
 app.config['CKEDITOR_FILE_UPLOADER'] = 'upload'
+## TODO: work out upload path for Heroku
+## TODO: create PDF to text uploader
 app.config['UPLOADED_PATH'] = os.path.join(basedir, 'uploads')
 app.config['CKEDITOR_HEIGHT'] = 250
 
 ## Connect to DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///timer.db')
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "asdflkj")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///personal.db')
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "lah*(P98&*(g7Wgg1")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -41,6 +43,12 @@ class TaskForm(FlaskForm):
     name = StringField('Task name:')
     info = CKEditorField('Task info')
     hours = IntegerField('Hours spent:')
+    submit = SubmitField()
+
+
+class RecipeForm(FlaskForm):
+    title = StringField('Recipe title:')
+    directions = CKEditorField('Directions:')
     submit = SubmitField()
 
 
@@ -68,17 +76,69 @@ class TaskList(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
 
+## Recipe table
+class Recipes(db.Model):
+    __tablename__ = 'recipes'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(256))
+    data_keywords = db.Column(db.String)
+    recipe_body = db.Column(db.String)
+    date_added = db.Column(db.String, default=datetime.today().strftime('%d-%m-%Y'))
+    image_url = db.Column(db.String)
+
+
 db.create_all()
 
 
 @app.route("/")
-@login_required
 def home():
+    return render_template("index.html")
+
+
+@app.route("/recipes")
+def recipes():
+    return render_template('recipes.html', recipes=Recipes.query.order_by(Recipes.date_added.asc()))
+
+
+@app.route("/recipe/<recipe_id>")
+def recipe(recipe_id):
+    return render_template("recipe.html", recipe=Recipes.query.get(recipe_id))
+
+
+@app.route("/add_recipe", methods=["GET", "POST"])
+@login_required
+def add_recipe():
+    form = RecipeForm()
+    if request.method == "GET":
+        return render_template("add_recipe.html", form=form)
+    else:
+        new_recipe_name = form.title.data
+        ckeditor_data = form.directions.data
+        ingredients = []
+        for i in ckeditor_data.splitlines():
+            if i == "<hr />":
+                break
+            else:
+                if 'Ingredients' not in i:
+                    ingredients.append(i.replace("<br />",""))
+        new_recipe = Recipes(title=new_recipe_name,
+                             recipe_body=ckeditor_data,
+                             data_keywords=json.dumps(ingredients))
+        print(ingredients)
+        print(type(ingredients))
+        db.session.add(new_recipe)
+        db.session.commit()
+        return redirect(url_for('recipes'))
+
+
+@app.route("/task_man")
+@login_required
+def taskman():
     tasks = current_user.tasks.all()
-    return render_template("index.html", tasks=tasks, current_user=current_user)
+    return render_template("task_man.html", tasks=tasks, current_user=current_user)
 
 
-@app.route("/start/<task_id>")
+@app.route("/start_task/<task_id>")
 def start(task_id):
     # Starts timer
     task = TaskList.query.get(task_id)
@@ -88,8 +148,7 @@ def start(task_id):
     return redirect(request.referrer)
 
 
-
-@app.route("/end/<task_id>")
+@app.route("/end_task/<task_id>")
 def end(task_id):
     # Ends timer and adds hours to db
     task = TaskList.query.get(task_id)
@@ -103,13 +162,12 @@ def end(task_id):
     return redirect(request.referrer)
 
 
-
-@app.route("/add", methods=["GET", "POST"])
+@app.route("/add_task", methods=["GET", "POST"])
 @login_required
 def add():
     form = TaskForm()
     if request.method == "GET":
-        return render_template("add.html", form=form)
+        return render_template("add_task.html", form=form)
     else:
         new_task_name = form.name.data
         ckeditor_data = form.info.data
@@ -120,14 +178,14 @@ def add():
         return redirect(url_for('home', tasks=tasks, current_user=current_user))
 
 
-@app.route("/edit/<task_id>", methods=["GET", "POST"])
+@app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 @login_required
 def edit(task_id):
     form = TaskForm()
     edit_task = TaskList.query.get(task_id)
     if request.method == "GET":
         form.info.data = edit_task.info
-        return render_template("edit.html", task=edit_task, form=form)
+        return render_template("edit_task.html", task=edit_task, form=form)
     else:
         if form.name.data == "":
             pass
@@ -148,7 +206,7 @@ def edit(task_id):
     return redirect(url_for('home', tasks=tasks, current_user=current_user))
 
 
-@app.route("/complete/<task_id>")
+@app.route("/complete_task/<task_id>")
 def complete(task_id):
     edit_task = TaskList.query.get(task_id)
     edit_task.completed = True
@@ -157,7 +215,7 @@ def complete(task_id):
     return redirect(request.referrer)
 
 
-@app.route("/delete/<task_id>")
+@app.route("/delete_task/<task_id>")
 def delete(task_id):
     delete_task = TaskList.query.get(task_id)
     db.session.delete(delete_task)
@@ -185,7 +243,7 @@ def upload():
 
 
 ## User handling functions
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/fZfI@^0lXOfRv2Ks&RI(1Ov4", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
         return render_template('register.html')
